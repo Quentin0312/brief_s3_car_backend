c2a864ec400db1b2dc2e363a721514f478526d6e
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-05-03 14:06:19 +0200

    Update controllers.py
    Essaie avec plusieurs features

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,12 +1,16 @@
+import numpy as np
 from sklearn import svm
+from sklearn.model_selection import train_test_split
+
 
 
 class tests:
     def testSKLearn():
-        X = [[0, 0], [2, 2]]
+        X = [[0, 0, 1], [2, 2, 3]]
         y = [0.5, 2.5]
         regr = svm.SVR()
         regr.fit(X, y)
 
-        result = regr.predict([[1, 1]])
+        result = regr.predict([[1, 1, 3]])
+        
         return result
