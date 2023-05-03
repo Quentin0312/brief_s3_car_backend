fa8132304c73d4d117e8bec257186601e00aa28f
Quentin PAYET <quentinpayet1203@gmail.com>
2023-05-03 07:53:46 +0400

    first commit

---
+++ datacsv/cleaning.py
@@ -0,0 +1,9 @@
+import pandas
+
+df = pandas.read_csv('./datacsv/car_price_prediction.csv')
+print(df.info())
+
+# TODO:
+# Vérif les doublons ID
+# valeurs manquantes
+# Valeurs abérantes
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Local installation
+
+```sh
+python -m venv .venv
+```
+
+```sh
+pip install -r requirements.txt
+```
