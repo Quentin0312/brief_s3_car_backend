bf796660499d49a31ff994236ef078fc4ab50dc2
Quentin PAYET <quentinpayet1203@gmail.com>
2023-05-03 08:52:33 +0400

    fait: verif doublons et valeurs manquantes

--- datacsv/cleaning.py
+++ datacsv/cleaning.py
@@ -1,9 +1,23 @@
-import pandas
+import pandas as pd
 
-df = pandas.read_csv('./datacsv/car_price_prediction.csv')
-print(df.info())
+df = pd.read_csv('./datacsv/car_price_prediction.csv')
+# print(df.info())
+
+
+# Vérif les doublons ID -----------Pas de doublon-------------------
+print("avant=> ", len(df.index))
+df.drop_duplicates(subset=['ID'], keep='first')
+print("après drop_duplicates=> ", len(df.index))
+
+
+# valeurs manquantes-------------------------------------
+df.dropna(inplace=True)
+print("après dropna=> ", len(df.index))
+
+# Ici il y a des valeurs manquantes mais elle ne sont pas vide => "_"
+df.replace('-', 'NA', inplace=True)
+df.drop(df[df['Levy'] == 'NA'].index, inplace=True)
+print("après replace '-'=>'NA' puis drop=> ", len(df.index))
 
 # TODO:
-# Vérif les doublons ID
-# valeurs manquantes
 # Valeurs abérantes
