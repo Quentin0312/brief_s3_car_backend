a2bbbbd88c06668d756b392b1ebe4b8445dacab6
Quentin PAYET <quentinpayet1203@gmail.com>
2023-05-10 18:35:36 +0400

    ajout cleaned_v1.csv contenant id

--- datacsv/cleaning_v1.py
+++ datacsv/cleaning_v1.py
@@ -1,11 +1,12 @@
 # TODO: Suppr les poublons
 #  Certains model nom doublé ex (x-trail x-trail)
-
+# fmt: off
 from cleaning_func import clean_manufacturer, clean_mileage, clean_price, clean_engine_volume, clean_doors, clean_model, clean_cylinders, clean_wheel, clean_leather_interior
 import pandas as pd
 import pandas
-alphabet_georgien = ['ა', 'ბ', 'გ', 'დ', 'ე', 'ვ', 'ზ', 'თ', 'ი', 'კ', 'ლ', 'მ', 'ნ', 'ო',
-                     'პ', 'ჟ', 'რ', 'ს', 'ტ', 'უ', 'ფ', 'ქ', 'ღ', 'ყ', 'შ', 'ჩ', 'ც', 'ძ', 'წ', 'ჭ', 'ხ', 'ჯ', 'ჰ']
+alphabet_georgien = ['ა', 'ბ', 'გ', 'დ', 'ე', 'ვ', 'ზ', 'თ', 'ი', 'კ', 'ლ', 'მ', 'ნ',
+                     'ო', 'პ', 'ჟ', 'რ', 'ს', 'ტ', 'უ', 'ფ', 'ქ', 'ღ', 'ყ', 'შ', 'ჩ',
+                     'ც', 'ძ', 'წ', 'ჭ', 'ხ', 'ჯ', 'ჰ']
 
 
 df = pd.read_csv('./datacsv/original.csv')
@@ -16,16 +17,17 @@ df = df[['Price', 'Manufacturer', 'Model', 'Prod. year', 'Category', 'Leather in
 
 print(len(df.index))
 
-
+# fmt: on
 # Valeurs abérantes
 print("avant suppr", str(len(df.index)))
 
-df = clean_manufacturer(df, ['სხვა', 'TESLA'],
-                        'Manufacturer')  # Manufacturer => 3 lignes
+df = clean_manufacturer(
+    df, ["სხვა", "TESLA"], "Manufacturer"
+)  # Manufacturer => 3 lignes
 df = clean_price(df, min=500, max=1000000)  # Price => 1664 lignes
 df = clean_mileage(df, min=500, max=1000000)  # Mileage => 677 lignes
 df = clean_engine_volume(df, 0.5, 8)  # Engine volume => 28 lignes
-df = clean_cylinders(df, min=2.0, max=12)  # Cylinders => 37 lignes
+df = clean_cylinders(df, min=3.0, max=12)  # Cylinders => 37 lignes
 df = clean_doors(df)  # Doors => 0 lignes
 df = clean_model(df)  # Model => 0 lignes
 df = clean_wheel(df)  # Wheel => 0 lignes
@@ -44,4 +46,7 @@ mini_poc_df = df.iloc[:, [0, 1, 3, 8]].copy()
 print(mini_poc_df.dtypes)
 
 # Enregistrer le nouveau csv
-mini_poc_df.to_csv('./datacsv/mini_poc.csv', index=False)
+# mini_poc_df.to_csv("./datacsv/mini_poc.csv", index=False)
+df["ID"] = df.index
+print(df.head())
+df.to_csv("./datacsv/cleaned_v1.csv", index=False)
