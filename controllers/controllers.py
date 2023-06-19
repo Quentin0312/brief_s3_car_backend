b9cc446dcd14c1d5885c9be1e71c3abb0a00ec58
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-19 13:28:33 +0400

    wip

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -19,31 +19,31 @@ class tests:
 
     def mini_poc_test():
         # Import
-        df = pd.read_csv('./datacsv/mini_poc.csv')
+        df = pd.read_csv("./datacsv/mini_poc.csv")
 
         # Encodage
-        col = df['Manufacturer']
+        col = df["Manufacturer"]
         enc = OneHotEncoder()
         enc.fit(col.values.reshape(-1, 1))
         encoded_col = enc.transform(col.values.reshape(-1, 1))
-        encoded_df = pd.DataFrame(
-            encoded_col.toarray(), columns=enc.categories_[0])
+        encoded_df = pd.DataFrame(encoded_col.toarray(), columns=enc.categories_[0])
         df_encoded = pd.concat([df, encoded_df], axis=1)
-        df_encoded.drop(columns=['Manufacturer'], inplace=True)
+        df_encoded.drop(columns=["Manufacturer"], inplace=True)
 
         print(df_encoded.head())
 
-        X = df_encoded.drop('Price', axis=1)
-        y = df_encoded['Price']
+        X = df_encoded.drop("Price", axis=1)
+        y = df_encoded["Price"]
 
         print(X.head())
         print(y.head())
 
         # Diviser les données en ensembles de formation et de test
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42)
+            X, y, test_size=0.2, random_state=42
+        )
 
-        model = svm.SVR(kernel='rbf')
+        model = svm.SVR(kernel="rbf")
 
         model.fit(X_train, y_train)
 
@@ -51,4 +51,9 @@ class tests:
 
         rmse = mean_squared_error(y_test, y_pred, squared=False)
         rscore = r2_score(y_test, y_pred)
-        return rscore
+        return {"rmse": rmse, "rscore": rscore}
+
+
+class estimate:
+    def estimate():
+        return "TODO"
--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import hug
 from hug.middleware import CORSMiddleware
-from controllers.controllers import tests
+from controllers.controllers import tests, estimate
+
 api = hug.API(__name__)
 # allow_origins à restreindre pour le déploiement
-api.http.add_middleware(CORSMiddleware(api, allow_origins=['*']))
+api.http.add_middleware(CORSMiddleware(api, allow_origins=["*"]))
 
 api_base_url = "/api"
 
 hug.get(api_base_url + "/sklearnTest", api=api)(tests.testSKLearn)
 hug.get(api_base_url + "/mini_poc_test", api=api)(tests.mini_poc_test)
+
+hug.post(api_base_url + "/estimate", api=api)(estimate.estimate)
